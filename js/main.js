851724d84b0c785c06a5510d8a5e6a36e1040e71
paulBit3 <paultechnologie@gmail.com>
2020-06-12 01:37:46 -0500

    First Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+/* 1. Proloder */
+
+(function ($)
+  { "use strict"
+  
+/* 1. Proloder */
+    $(window).on('load', function () {
+      $('#preloader-active').delay(450).fadeOut('slow');
+      $('body').delay(450).css({
+        'overflow': 'visible'
+      });
+    });
+
+})(jQuery);
+
+ 
